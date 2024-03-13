8847d71bf11b8cbed68af3be48e5d8cfadf4daec
lokeshsainisamcrit <lokesh@samcrit.com>
2024-03-13 12:46:24 +0530

    validate string is start with

--- stringcalculator.rb
+++ stringcalculator.rb
@@ -1,6 +1,10 @@
 def add(numbers)
     delimiter = ','
-    delimiter_line, numbers = numbers.split("\n", 2)
+
+    if numbers.start_with?("//")
+        delimiter_line, numbers = numbers.split("\n", 2)
+        delimiter = delimiter_line[2..-1]
+    end
     nums = numbers.split(/#{Regexp.escape(delimiter)}|\n/)
     negatives = nums.select { |num| num.to_i < 0 }
     nums.map(&:to_i).inject(0) { |sum, num| num <= 1000 ? sum + num : sum }
